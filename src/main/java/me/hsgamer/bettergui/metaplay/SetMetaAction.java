3ffd0861d40ce1838e6bca57afee0db5afb74d9c
hsgamer <huynhqtienvtag@gmail.com>
2024-07-17 00:41:08 +0700

    update plugin

--- src/main/java/me/hsgamer/bettergui/metaplay/SetMetaAction.java
+++ src/main/java/me/hsgamer/bettergui/metaplay/SetMetaAction.java
@@ -1,10 +1,9 @@
 package me.hsgamer.bettergui.metaplay;
 
-import me.hsgamer.bettergui.api.action.Action;
-import me.hsgamer.bettergui.api.menu.Menu;
 import me.hsgamer.bettergui.builder.ActionBuilder;
-import me.hsgamer.bettergui.util.StringReplacerApplier;
-import me.hsgamer.hscore.bukkit.scheduler.Scheduler;
+import me.hsgamer.bettergui.util.SchedulerUtil;
+import me.hsgamer.hscore.action.common.Action;
+import me.hsgamer.hscore.common.StringReplacer;
 import me.hsgamer.hscore.task.element.TaskProcess;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
@@ -15,37 +14,34 @@ import java.util.UUID;
 
 public class SetMetaAction implements Action {
     private final MetaPlay addon;
-    private final Menu menu;
     private final String value;
     private final String name;
     private final boolean isNumber;
 
     public SetMetaAction(MetaPlay addon, ActionBuilder.Input input) {
         this.addon = addon;
-        this.menu = input.menu;
-        this.value = input.value;
+        this.value = input.getValue();
         List<String> optionList = input.getOptionAsList();
         this.name = !optionList.isEmpty() ? optionList.get(0) : "";
         this.isNumber = optionList.size() > 1 && optionList.get(1).equalsIgnoreCase("number");
     }
 
     @Override
-    public void accept(UUID uuid, TaskProcess process) {
+    public void apply(UUID uuid, TaskProcess process, StringReplacer stringReplacer) {
         Player player = Bukkit.getPlayer(uuid);
         if (player == null) {
             process.next();
             return;
         }
-        Scheduler.current().sync().runEntityTaskWithFinalizer(player, () -> {
-            String previousValue = addon.getMetadataValue(player, name).map(MetadataValue::asString).orElse(isNumber ? "0" : "");
-            String finalValue = value.replace("{value}", previousValue);
-            finalValue = StringReplacerApplier.replace(finalValue, uuid, this);
-            addon.setMetadataValue(player, name, finalValue);
+        SchedulerUtil.entity(player).run(() -> {
+            try {
+                String previousValue = addon.getMetadataValue(player, name).map(MetadataValue::asString).orElse(isNumber ? "0" : "");
+                String finalValue = value.replace("{value}", previousValue);
+                finalValue = stringReplacer.replaceOrOriginal(finalValue, uuid);
+                addon.setMetadataValue(player, name, finalValue);
+            } finally {
+                process.next();
+            }
         }, process::next);
-    }
-
-    @Override
-    public Menu getMenu() {
-        return menu;
     }
 }
